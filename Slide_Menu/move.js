26aa2f168865b1bc4b99d023194becf621a05339
kvgess <kvgess@gmail.com>
2018-07-12 17:40:07 -0700

    Add files via upload

---
+++ Slide_Menu/move.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "75%";
+    document.getElementById("main").style.marginLeft = "75%";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
